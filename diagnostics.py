a12fd6739b4c701b264c5624ecf55f93b7a514f7
Chloe Becker <chloe.a.becker@gmail.com>
2018-10-03 18:57:31 -0500

    fixed syntax error

--- diagnostics.py
+++ diagnostics.py
@@ -1,12 +1,16 @@
 import datetime
 
-main():
+def diagnostics():
+	diagnostic = []
 	gwId = 1
+	diagnostic.append(gwId)
 	timestamp = datetime.datetime.now()
+	diagnostic.append(str(timestamp))
+
 
 	print("gwId")
 	print(gwId)
 	print("timestamp")
-	print(timestamp)
+	print(str(timestamp))
 
-main()
+diagnostics()
