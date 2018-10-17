cc9e18a3ca5a4ad807fb0fce496b0c7f4caee02e
Chloe Becker <chloe.a.becker@gmail.com>
2018-10-16 23:49:35 -0500

    added cpu temp and battery functions

--- diagnostics.py
+++ diagnostics.py
@@ -1,6 +1,7 @@
 import time
+import psutil
 
-options = ['timestamp()']
+options = ['timestamp()','cpuBattery()', 'cpuTemps()']
 
 class Diagnostics():
 
@@ -8,8 +9,23 @@ class Diagnostics():
 		global options
 		return options
 
+	# returns time in epoch format
 	def timestamp():
 		timestamp = time.time()
 		print("timestamp")
 		print(timestamp)
 		return timestamp
+
+	# returns current, high, and critical temps in CELCIUS => float
+	def cpuTemps():
+		cpuTemps = psutil.temperatures()
+		print("cpuTemps")
+		print(cpuTemps)
+		return cpuTemps
+
+	# returns percent => int, secsleft => float, power_plugged => boolean
+	def cpuBattery():
+		cpuBattery = psutil.sensors_battery()
+		print("cpuBattery")
+		print(cpuBattery)
+		return cpuBattery
