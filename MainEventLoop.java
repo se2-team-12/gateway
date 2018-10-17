d81a8187606cd9b1bc75fbb66ffcdb73fa32008e
balsumae <43450007+balsumae@users.noreply.github.com>
2018-10-17 12:10:46 -0500

    Update and rename Loop.java to MainEventLoop.java

---
+++ MainEventLoop.java
@@ -0,0 +1,70 @@
+package http_requests;
+ 
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class MainEventLoop {
+
+	public static void main(String[] args) 
+	{
+	   
+		Runnable runnable = new Runnable() {
+	    		public void run() {
+	    			JSONObject urlParametersJson = null;
+	    			try
+	    			{
+	    				urlParametersJson=http_requests.ReadPython.readPython();
+	    			} catch (IOException | ParseException e1)
+	    			{
+	    				e1.printStackTrace();
+	    			}
+	    			System.out.println(urlParametersJson.toString());
+	    			//String url = "https://balsumae.create.stedwards.edu/cosc3326/insertGateway.php";
+	    			//String url = "https://team12.softwareengineeringii.com/api/gateway";
+	    			String url="https://itsonlytemporary.softwareengineeringii.com/api/gateway";
+	    			try {
+						System.out.println(http_requests.Requests.sendPost(url, urlParametersJson));
+						String response = http_requests.Requests.sendPost(url, urlParametersJson);
+						//(1) we create an instance of JSONParser
+						//(2)we create a JSONObject by parsing the FileReader of our .json file.
+						//This JSONObject contains a collection of key-value pairs,
+						//from which we can get every value of the json file. 
+						JSONParser jsonParser = new JSONParser();
+						JSONObject jsonObject = (JSONObject) jsonParser.parse(response);
+						// get a boolean from the JSON object
+						/*boolean onDemand = (boolean) jsonObject.get("onDemand");
+						 *String  typeOfDemand = jsonObject.get("typeOfDemand");
+						 *
+						System.out.println("The onDemand is: " + onDemand);
+						
+						if(onDemand)
+						{
+							
+						}*/
+						
+						String  status= (String) jsonObject.get("Status");
+						if(status.equals("OK"))
+						{
+							System.out.println("status is OK ");
+						}
+						
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+	        }
+	    };
+	    
+	    ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+	    service.scheduleAtFixedRate(runnable, 0, 5, TimeUnit.SECONDS);	
+	}
+	
+	
+	
+
+}
